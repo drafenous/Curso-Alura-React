8a082ba76a5195b7a24c1e0c400048899ba83c41
Rodrigo R. Almeidam <rodrigo.roberto.almeida@gmail.com>
2020-03-29 21:25:54 -0300

    Alterado o projeto para MaterialUI, refatorado os componentes e refatorado o ApiService

---
+++ src/Pages/NotFound/NotFound.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react';
+import Header from '../../Components/Header/Header';
+
+const NotFound = () => {
+    return (
+        <Fragment>
+            <Header />
+            <h1>404 - Not Found</h1>
+        </Fragment>
+    )
+}
+
+export default NotFound;
